7d2b6eaf2c06618e5922924bcf43bed523a59bf4
furkanesennn <furkanesen1900@gmail.com>
2023-03-07 13:53:43 +0300

    add:first aditions

--- theaterclub/website/urls.py
+++ theaterclub/website/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+# from django.views.generic import TemplateView
+
 from . import views
 
 urlpatterns = [
+    path('application', views.ApplicationView.as_view(template_name='application.html'), name = 'application-page'),
     path('', views.index, name = 'index'),
-    path('application', views.application, name = 'application-page'),
 ]
